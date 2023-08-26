e55674800d80bb6199e894c9e4376b1980a080a1
YELYZAVETA ZINCHUK <107926338+mavka13@users.noreply.github.com>
2023-08-26 22:46:32 +0300

    add border

--- js/task-06.js
+++ js/task-06.js
@@ -1,34 +1,10 @@
-const validationInput = document.querySelector("#validation-input");
+const inputElem = document.querySelector("#validation-input");
 
-const handleBlur = event => {
+const handleBlur = (event) => {
+ let inputText = event.currentTarget.value;
 
-    if(event.target.value.length > Number(event.target.dataset.length) ){
-      validationInput.className = "invalid"  
-    } else{
-        validationInput.className = "valid"
-    }
-
-
+ inputText.length !== Number(inputElem.dataset.length)
+   ? inputElem.classList.add("invalid")
+   : inputElem.classList.remove("invalid") || inputElem.classList.add('valid');
 }
-
-validationInput.addEventListener("blur", handleBlur);
-
-// const requiredLength = parseInt(validationInput.dataset.length);
-//   const inputValue = validationInput.value.trim();
-  
-//   if (inputValue.length === requiredLength) {
-//     validationInput.classList.remove("invalid");
-//     validationInput.classList.add("valid");
-//   } else {
-//     validationInput.classList.remove("valid");
-//     validationInput.classList.add("invalid");
-//   }
-//   const inputValue = e.currentTarget;
-//   const requiredLength = Number(input1.dataset.length);
-//   if (inputValue.value.length === requiredLength) {
-//     inputValue.classList.remove("invalid");
-//     inputValue.classList.add("valid");
-//   } else {
-//     inputValue.classList.remove("valid");
-//     inputValue.classList.add("invalid");
-//   }
+inputElem.addEventListener("blur", handleBlur);
